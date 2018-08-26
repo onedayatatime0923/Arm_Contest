df8b869d8214904631c28b1d095c4e0eb06775ee
kevin <onedayatatime0923@gmail.com>
2018-08-26 16:24:18 +0800

    all

--- LSM9DS0/visualizer.py
+++ LSM9DS0/visualizer.py
@@ -4,18 +4,14 @@ assert np
 
 
 class Visualizer():
-    def __init__(self, name = ['Accel X', 'Accel Y', 'Accel Z', 'Gyro X', 'Gyro Y', 'Gyro Z'], memorySize = 10):
-        self.name = name
-        self.n = len(name)
-        self.memorySize = memorySize
-        self.data = np.empty((self.n))
+    def __init__(self):
+        pass
     def __call__(self, data):
         message = '\x1b[2K\r'
-        for i in range(self.n):
-            print(self.name[i], data[i])
-            message += '{}: {:.4f} | '.format(
-                self.name[i], float(data[i]))
-        self.data = np.append(self.data, data, axis = 1)
+        for n in range(data):
+            print(n, data[n])
+            message += '{} X: {} Y: {} Z:{} | '.format(
+                n, data[n][0], data[n][1], data[n][2])
 
         print(message, flush = True)
 
