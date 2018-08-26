5e8ff3f7874658ca17999eb08b3b41d3ccd98360
kevin <onedayatatime0923@gmail.com>
2018-08-26 16:29:55 +0800

    all

--- LSM9DS0/visualizer.py
+++ LSM9DS0/visualizer.py
@@ -9,7 +9,6 @@ class Visualizer():
     def __call__(self, data):
         message = '\x1b[2K\r'
         for n in range(data):
-            print(n, data[n])
             message += '{} X: {} Y: {} Z:{} | '.format(
                 n, data[n][0], data[n][1], data[n][2])
 
