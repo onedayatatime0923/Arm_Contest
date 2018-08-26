8fa23ca4e8b2c977ec833aada2bc2024816a9200
kevin <onedayatatime0923@gmail.com>
2018-08-26 19:19:59 +0800

    all

--- LSM9DS0/main.py
+++ LSM9DS0/main.py
@@ -6,10 +6,11 @@ from painter import Painter
 from sensor import Sensor
 from filter import Filter
 
+name = ['Accel X', 'Accel Y', 'Accel Z', 'Gyro X', 'Gyro Y', 'Gyro Z', 'Magnetic X', 'Magnetic Y', 'Magnetic Z']
 sensor = Sensor("COM3")
 filter = Filter(9,9)
-visualizer = Visualizer()
-painter = Painter()
+visualizer = Visualizer( name = name)
+painter = Painter(name = name)
 
 def main():
     while True:
