77fe49ca952c22d964d838d532f87d8436dfc2e6
eric86103 <eric86103@hotmail.com>
2018-08-26 16:53:35 +0800

    123

--- LSM9DS0/main.py
+++ LSM9DS0/main.py
@@ -2,22 +2,20 @@
 from visualizer import Visualizer
 from painter import Painter
 from sensor import Sensor
-import multiprocessing
+import threading as td
 #from filter import Filter
 
 sensor = Sensor("/dev/cu.usbmodem1413")
 #filter = Filter(6,6)
 visualizer = Visualizer()
 painter = Painter()
-process = multiprocessing.Process(target=painter.plot,args=())
-process.start()
 
-'''
 def main():
     while True:
         data = sensor.read()
         #data = filter.update(sensor.read())
         visualizer(data)
-        #painter(data)
-main()
-'''
+        painter(data)
+
+td.Thread(target=main).start()
+painter.plot()
