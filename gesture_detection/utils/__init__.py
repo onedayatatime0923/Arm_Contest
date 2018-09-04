ffc131a0b5086f9d9eeab6901e6e0ed4a24345ed
kevin <onedayatatime0923@gmail.com>
2018-09-04 18:41:46 +0800

    all

--- gesture_detection/utils/__init__.py
+++ gesture_detection/utils/__init__.py
@@ -1,8 +1,8 @@
 
 #from utils.accu_eval import AccuEval
-from utils.loss import CrossEntropyLoss2d, Distance, GANLoss
+#from utils.loss import CrossEntropyLoss2d, Distance, GANLoss
 from utils.timer import Timer
 
 #assert AccuEval
-assert CrossEntropyLoss2d and Distance and GANLoss
+#assert CrossEntropyLoss2d and Distance and GANLoss
 assert Timer
