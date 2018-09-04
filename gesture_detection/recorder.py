22942ed1c3debec19fc0b3cf1f07458a9683167d
kevin <onedayatatime0923@gmail.com>
2018-09-04 22:27:17 +0800

    all

--- gesture_detection/recorder.py
+++ gesture_detection/recorder.py
@@ -12,7 +12,7 @@ opt = parser.parse()
 sensor = Sensor(opt.port)
 filter = Filter(opt.n, opt.n)
 visualizer = SensorVisualizer(name = opt.repr)
-painter = Painter(name = opt.repr, display = opt.display, memorySize = opt.memorySize, ylim = opt.ylim )
+painter = Painter(repr = opt.repr, display = opt.display, memorySize = opt.memorySize, ylim = opt.ylim )
 recorder = Recorder(opt)
 
 print("action: {}".format(opt.action))
