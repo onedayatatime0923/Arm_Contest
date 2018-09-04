31780d1838d367d41e6080d16ab26463411abe1d
kevin <onedayatatime0923@gmail.com>
2018-09-04 20:51:31 +0800

    all

--- gesture_detection/options/main_options.py
+++ gesture_detection/options/main_options.py
@@ -7,7 +7,7 @@ from options.base_options import BaseOptions
 
 class MainOptions(BaseOptions):
     def __init__(self):
-        BaseOptions.__init__()
+        BaseOptions.__init__(self)
     def initialize(self, parser):
         parser = BaseOptions.initialize(self, parser)
         # ---------- Define Network ---------- #
