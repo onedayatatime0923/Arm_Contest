a9ccff3992d3e053fd08ed0b295f82166cd82f02
kevin <onedayatatime0923@gmail.com>
2018-09-05 20:43:11 +0800

    all

--- gesture_detection/recorder/__init__.py
+++ gesture_detection/recorder/__init__.py
@@ -32,10 +32,10 @@ class Recorder():
             path = os.path.join(self.opt.actionDir, '{}.npy'.format(index))
             index += 1
         print('saved data to {}...'.format(path))
-        #raw_input()
         np.save(path, np.array([self.X, self.Y]))
         self.X = []
         self.Y = []
+        exit()
 
     def dump_split(self):
         index = 0
