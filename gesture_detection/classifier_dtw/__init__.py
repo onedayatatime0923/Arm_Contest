b4362261c8f014f07cff802f7b81a693c44d38da
kevin <onedayatatime0923@gmail.com>
2018-09-06 19:46:31 +0800

    all

--- gesture_detection/classifier_dtw/__init__.py
+++ gesture_detection/classifier_dtw/__init__.py
@@ -2,10 +2,15 @@
 import os
 from dtw import dtw
 import numpy as np
+from numpy import linalg as LA
 
 assert dtw
 
 class Classifier:
+    def __init__(self, noOps = 'None'):
+        self.noOps = noOps
+        self.action = None
+        self.data = None
     def read(self):
         action = []
         data = []
@@ -16,7 +21,17 @@ class Classifier:
                 fileList = os.listdir(path) if os.path.exists(path) else []
                 for f in fileList:
                     action.append(act)
-                    data.append(np.load(os.path.join(path,f)))
+                    data.append(np.load(os.path.join(path,f))[0])
         self.action = action
         self.data = data
-
+    def predict(self, target):
+        target = np.array(target)
+        score = []
+        for d in self.data:
+            score.append(dtw(d, target, dist=lambda x, y: LA.norm(x - y, ord=1))[0])
+        score = np.array(score)
+        print(min(np.array(score)))
+        if min(np.array(score)) < 98.:
+            return self.action[np.argmax(score)]
+        else:
+            return self.noOps
--- gesture_detection/predict.py
+++ gesture_detection/predict.py
@@ -1,11 +1,8 @@
-import numpy as np
-import os
-from dtw import dtw
-from numpy import linalg as LA
+
 
 import torch
-import multiprocessing as mp
 from torch.autograd import Variable
+
 from options import MainOptions
 from sensor import Sensor
 from filter import Filter
@@ -13,6 +10,7 @@ from visualizer import SensorVisualizer
 from models import createModel
 from utils import convert
 from speech import Speech
+from classifier_dtw import Classifier
 
 parser = MainOptions()
 opt = parser.parse()
@@ -20,42 +18,15 @@ sensor = Sensor(opt.port)
 filter = Filter(opt.n, opt.n)
 visualizer = SensorVisualizer(repr = opt.repr)
 speech = Speech()
+classifier = Classifier()
 
 model = createModel(opt)
 model.setup(opt)
 model.eval()
 
-def reading_ref():
-    action_list = []
-    ref_act = []
-    action = []
-    with open("./utils/vocabulary/record.txt", 'r') as f:
-        for line in f:
-            action_list.append(line.strip())
-    for act in action_list:
-        if(os.path.exists("./data/{}/".format(act))):
-            ref_act.append(np.array([data for data in np.load("data/{}/0.npy".format(act))[0]]))
-            action.append(act)
-    return action, ref_act
-
-def predict(target, ref_act):
-    score = []
-    for ref in ref_act:
-        score.append(dtw(ref, target, dist=lambda x, y: LA.norm(x - y, ord=1))[0])
-    if min(np.array(score)) < 98.:
-        print(min(np.array(score)))
-        return score.index(min(np.array(score)))
-    else:
-        print(min(np.array(score)))
-        return -1.
-
-
-            
 
 
 def main():
-    action_list, ref_act = reading_ref()
-    #print(ref_act)
     target = []
     while True:
         data = sensor.read()
@@ -63,20 +34,14 @@ def main():
         x = Variable(convert(torch.FloatTensor(data)))
         signal = model.predict(x)
         if signal:
-            target.append(data)
-            index = predict(target, ref_act)
-            if index != -1.:
-                print(action_list[index])
-                p1 = mp.Process(target=speech, args=(action_list[index],))
-                p1.start()
+            operate = classifier.predict(target)
+            print(operate)
+            if operate != 'None':
+                speech(operate)
                 target = []
-            else:
-                #print("No act")
-                pass
         else:
             print("Stop")
             target = []
-            pass
         #visualizer(data)
     
 if(__name__ == '__main__'):
