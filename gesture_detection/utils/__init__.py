3c33602a361e9ebb4110a71a960cf32f68e159b4
kevin <onedayatatime0923@gmail.com>
2018-09-10 01:50:18 +0800

    all

--- gesture_detection/utils/__init__.py
+++ gesture_detection/utils/__init__.py
@@ -3,11 +3,11 @@
 from .accu_eval import AccuEval
 from .loss import CrossEntropyLoss2d, Distance, GANLoss
 from .timer import Timer
-from .utils import convert
+from .utils import convert, Queue
 from .vocabulary import Vocabulary
 
 assert AccuEval
 assert CrossEntropyLoss2d and Distance and GANLoss
 assert Timer
-assert convert
+assert convert and Queue
 assert Vocabulary
--- gesture_detection/utils/utils.py
+++ gesture_detection/utils/utils.py
@@ -1,4 +1,16 @@
 
+import torch
+
 def convert(data, n):
     data = data.view(1,n)
     return data
+
+class Queue:
+    def __init__(self, n, dim, device):
+        self.n = n
+        self.dim = dim
+        self.device = device
+        self.data = torch.zeros(n, dim).to(device)
+    def __call__(self, data):
+        self.data = torch.cat([self.data[1:,:],convert(data.to(self.device), self.dim)],0)
+        return self.data
