39082e44de2a9f9f8474d3b3dde9040f6847c3ea
kevin <onedayatatime0923@gmail.com>
2018-09-09 23:52:05 +0800

    all

--- gesture_detection/models/network/__init__.py
+++ gesture_detection/models/network/__init__.py
@@ -9,18 +9,21 @@ class Classifier(nn.Module):
         super(Classifier, self).__init__()
         self.opt = opt
         self.feature = nn.Sequential(
-            nn.BatchNorm1d(len(opt.input)),
+            GaussianNoise(device = opt.device),
             nn.Linear(len(opt.input), opt.ncf),
             nn.BatchNorm1d(opt.ncf),
             nn.LeakyReLU(0.2, inplace=True),
+            GaussianNoise(device = opt.device),
             nn.Dropout(opt.dropout),
             nn.Linear(opt.ncf, opt.ncf * 2),
             nn.BatchNorm1d(opt.ncf * 2),
             nn.LeakyReLU(0.2, inplace=True),
+            GaussianNoise(device = opt.device),
             nn.Dropout(opt.dropout),
             nn.Linear(opt.ncf * 2, opt.ncf * 4),
             nn.BatchNorm1d(opt.ncf * 4),
             nn.LeakyReLU(0.2, inplace=True),
+            GaussianNoise(device = opt.device),
             nn.Dropout(opt.dropout),
             nn.Linear(opt.ncf * 4, 1),
             nn.Sigmoid())
@@ -28,3 +31,30 @@ class Classifier(nn.Module):
         x = torch.index_select(x, 1, torch.LongTensor(self.opt.input))
         x = self.feature(x)
         return x
+
+class GaussianNoise(nn.Module):
+    """Gaussian noise regularizer.
+
+    Args:
+        sigma (float, optional): relative standard deviation used to generate the
+            noise. Relative means that it will be multiplied by the magnitude of
+            the value your are adding the noise to. This means that sigma can be
+            the same regardless of the scale of the vector.
+        is_relative_detach (bool, optional): whether to detach the variable before
+            computing the scale of the noise. If `False` then the scale of the noise
+            won't be seen as a constant but something to optimize: this will bias the
+            network to generate vectors with smaller values.
+    """
+
+    def __init__(self, sigma=0.1, is_relative_detach=True, device = 0):
+        super().__init__()
+        self.sigma = sigma
+        self.is_relative_detach = is_relative_detach
+        self.noise = torch.tensor(0).to(device)
+
+    def forward(self, x):
+        if self.training and self.sigma != 0:
+            scale = self.sigma * x.detach() if self.is_relative_detach else self.sigma * x
+            sampled_noise = self.noise.repeat(*x.size()).normal_() * scale
+            x = x + sampled_noise
+        return x 
