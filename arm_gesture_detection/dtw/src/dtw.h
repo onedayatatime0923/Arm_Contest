8921a864a592b0188274f431c98728833d835e22
eric86103 <eric86103@hotmail.com>
2018-10-02 21:02:51 +0800

    123

--- arm_gesture_detection/dtw/src/dtw.h
+++ arm_gesture_detection/dtw/src/dtw.h
@@ -1,29 +1,3 @@
-/*
-Copyright (c) 2014, Calder Phillips-Grafflin (calder.pg@gmail.com)
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-* Redistributions of source code must retain the above copyright notice, this
-  list of conditions and the following disclaimer.
-
-* Redistributions in binary form must reproduce the above copyright notice,
-  this list of conditions and the following disclaimer in the documentation
-  and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <vector>
@@ -44,7 +18,6 @@ private:
     size_t y_dim_;
     bool initialized_;
 
-    void Initialize(size_t x_size, size_t y_size);
 
     inline size_t GetDataIndex(size_t x, size_t y)
     {
@@ -72,6 +45,7 @@ public:
     ~SimpleDTW() {}
 
     double EvaluateWarpingCost(std::vector<std::vector<double> > sequence_1, std::vector<std::vector<double> > sequence_2);
+    void Initialize(size_t x_size, size_t y_size);
 
 };
 
